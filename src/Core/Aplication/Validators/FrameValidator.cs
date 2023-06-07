7e3b726961a4376136e6708a1891aaee10203e03
Mayra Bautista <mayra.bautista@sprbrk.com>
2023-06-07 13:59:41 -0400

    Add Frame Validator

---
+++ src/Core/Aplication/Validators/FrameValidator.cs
@@ -0,0 +1,15 @@
+﻿using BowlingGame.Core.Domain.Models;
+using FluentValidation;
+
+namespace BowlingGame.Core.Aplication.Validators
+{
+    public class FrameValidator : AbstractValidator<Frame>
+    {
+        public FrameValidator()
+        {
+            RuleFor(x => x.GameId)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}

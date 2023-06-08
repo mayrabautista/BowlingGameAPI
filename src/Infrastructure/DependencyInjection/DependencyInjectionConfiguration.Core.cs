44ba389130038d51d80b7838d238b32b54895381
Mayra Bautista <mayra.bautista@sprbrk.com>
2023-06-08 19:47:09 -0400

    move core injection to a partial class

---
+++ src/Infrastructure/DependencyInjection/DependencyInjectionConfiguration.Core.cs
@@ -0,0 +1,25 @@
+﻿using BowlingGame.Core.Aplication.Services;
+using BowlingGame.Core.Aplication.Validators;
+using BowlingGame.Core.Domain.Abstractions;
+using BowlingGame.Core.Domain.Models;
+using FluentValidation;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static partial class DependencyInjectionConfiguration
+    {
+        private static IServiceCollection AddBowlingGameServices(this IServiceCollection services)
+        {
+            services.AddScoped<IGamesService, GamesService>();
+            services.AddScoped<IFramesService, FramesService>();
+            return services;
+        }
+
+        private static IServiceCollection AddBowlingGameValidators(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<Game>, GameValidator>();
+            services.AddScoped<IValidator<Frame>, FrameValidator>();
+            return services;
+        }
+    }
+}
--- src/Infrastructure/DependencyInjection/DependencyInjectionConfiguration.cs
+++ src/Infrastructure/DependencyInjection/DependencyInjectionConfiguration.cs
@@ -1,17 +1,4 @@
-﻿using BowlingGame.Core.Aplication.Services;
-using BowlingGame.Core.Aplication.Validators;
-using BowlingGame.Core.Domain.Abstractions;
-using BowlingGame.Core.Domain.Models;
-using BowlingGame.Infrastructure.Mongo.Interfaces;
-using BowlingGame.Infrastructure.Mongo.Models;
-using BowlingGame.Infrastructure.Mongo.Repositories;
-using BowlingGame.Infrastructure.MSSql;
-using BowlingGame.Infrastructure.MSSql.Repositories;
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
-using MongoDB.Driver;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -20,6 +7,8 @@ namespace Microsoft.Extensions.DependencyInjection
        public static IServiceCollection AddBowlingGame(this IServiceCollection services,IConfiguration configuration)
         {
             services.AddBowlingGameServices();
+            services.AddBowlingGameValidators();
+
             string? dbStrategy = configuration["DatabaseStrategy"];
             if(dbStrategy == "Sql")
             {
@@ -30,22 +19,6 @@ namespace Microsoft.Extensions.DependencyInjection
                 services.AddBowlingGameMongo(configuration);
             }
 
-            services.AddBowlingGameValidators();
-            return services;
-        }
-
-
-        private static IServiceCollection  AddBowlingGameServices(this IServiceCollection services)
-        {
-            services.AddScoped<IGamesService, GamesService>();
-            services.AddScoped<IFramesService, FramesService>();
-            return services;
-        }
-    
-        private static IServiceCollection AddBowlingGameValidators(this IServiceCollection services)
-        {
-            services.AddScoped<IValidator<Game>, GameValidator>();
-            services.AddScoped<IValidator<Frame>, FrameValidator>();
             return services;
         }
     }
